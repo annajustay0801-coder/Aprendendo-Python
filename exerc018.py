6128883afd49f242b7288cc60e1e8f8c321f4afc
unknown <annajustay0801@gmail.com>
2026-08-10 14:18:41 -0300

    exercicio 18

---
+++ exerc018.py
@@ -0,0 +1,5 @@
+import math
+an = float(input('Digite o tamanho de um ângulo:'))
+print('O valor do SENO é {:.2f}'.format(math.sin(math.radians(an))))
+print('O valor do COSSENO é {:.2f}'.format(math.cos(math.radians(an))))
+print('O valor da TANGENTE é {:.2f}'.format(math.tan(math.radians(an))))
