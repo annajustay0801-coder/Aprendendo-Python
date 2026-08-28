f883beccbfba492fe3e6b2ec15ff7df0075e90f3
unknown <annajustay0801@gmail.com>
2026-08-28 18:24:17 -0300

    exercicio 23

---
+++ exerc023.py
@@ -0,0 +1,10 @@
+número = int(input('Digite um número: '))
+u = número // 1 % 10
+d = número // 10 % 10
+c = número // 100 % 10
+m = número // 1000 % 10
+print('Analisando o número {}'.format(número))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
